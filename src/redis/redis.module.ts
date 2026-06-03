203371675406d8d5882c6d509d678ca2b67d029c
Ronik <ronik.kalanouria@gmail.com>
2026-06-03 15:17:46 +0530

    fix: update Redis configuration to use environment variables for improved security

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,28 +1,26 @@
+import { createKeyv } from '@keyv/redis';
 import { CacheModule } from '@nestjs/cache-manager';
 import { Global, Module } from '@nestjs/common';
-import KeyvRedis from '@keyv/redis';
 
 @Global()
 @Module({
   imports: [
     CacheModule.registerAsync({
       isGlobal: true,
-
       useFactory: () => {
-        console.log('Redis Host:', process.env.REDIS_HOST);
+        const redisUrl = process.env.REDIS_PASSWORD
+          ? `rediss://default:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`
+          : `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`;
+
+        console.log('Redis URL:', redisUrl);
 
         return {
-          stores: [
-            new KeyvRedis(
-              `redis://:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
-            ),
-          ],
+          stores: [createKeyv(redisUrl)],
           ttl: 60_000,
         };
       },
     }),
   ],
-
   exports: [CacheModule],
 })
 export class RedisModule {}
